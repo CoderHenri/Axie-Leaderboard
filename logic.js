406b6e8a7db9c481a4d19a845e4aea399c848909
Henri0812 <p.metzner@gmx.at>
2020-11-07 19:34:25 +0100

    beginnings

---
+++ logic.js
@@ -0,0 +1,26 @@
+var NameArray = [];
+
+async function ReadTextFile() {
+  NameArray = await AsyncTextReader();
+  GetLandData('Genesis');
+}
+
+function AsyncTextReader() {
+  return new Promise(function (resolve, reject) {
+      var objXMLhttp = new XMLHttpRequest()
+      objXMLhttp.open("GET", './Textfiles/Profile_Loom_Eth_Addresses.txt', true);
+      objXMLhttp.send();
+      objXMLhttp.onreadystatechange = function(){
+      if (objXMLhttp.readyState == 4){
+        if(objXMLhttp.status == 200) {
+          var TestParse = objXMLhttp.responseText;
+          TestParse = JSON.parse(TestParse);
+          return resolve(TestParse);
+        } else {
+          return resolve("error");
+        }
+      }
+    }
+  });
+
+}
